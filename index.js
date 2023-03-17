6f84496d69bd40f1e9809cb85f307f1ef7de8691
ismaildevv@gmail.com <ismaildevv@gmail.com>
2023-03-17 17:26:39 +0100

    added post endpoint for /api/fileanalyse

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const app = express();
 
-var app = express();
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,6 +15,18 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file
+  const fileName = file.originalname
+  const fileSize = file.size
+  const fileType = file.mimetype
+
+  res.json({
+    name: fileName,
+    type: fileType,
+    size: fileSize
+  });
+});
 
 
 const port = process.env.PORT || 3000;
